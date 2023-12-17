b51a9b1f8f06cfd9e1ac57a99fcec244050a71bf
MYEONGJUN KIM <117059083+chorongs@users.noreply.github.com>
2023-12-18 08:46:28 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+
+                int thailand = sc.nextInt();
+                int korea = thailand - 543;
+
+                System.out.println(korea);
+
+                sc.close();
+
+
+    }
+}
