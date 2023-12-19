584777f3b03cc07ebdff45ab0682dd041e61d11d
MYEONGJUN KIM <117059083+chorongs@users.noreply.github.com>
2023-12-19 16:41:33 +0900

    [level unrated] Title: 각도기, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120829. 각도기/각도기.java
+++ 프로그래머스/unrated/120829. 각도기/각도기.java
@@ -1,15 +1,9 @@
 class Solution {
     public int solution(int angle) {
-        int answer = 0;
-        if ( 0 < angle && angle < 90) {
-            answer  = 1;
-        } else if ( angle == 90 ) {
-            answer = 2;
-        } else if ( 90 < angle && angle < 180 ) {
-            answer = 3;
-        } else  {
-            answer = 4;
-        }
-        return answer;
+        return angle > 0 && angle < 90 ? 1 : 
+               angle == 90 ? 2 : 
+               angle > 90 && angle < 180 ? 3 : 
+               angle == 180 ? 4 : 
+               0;
     }
 }
