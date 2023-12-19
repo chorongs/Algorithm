ec78b99a3fc78e841cd2e3400b0eb389ff22faa9
MYEONGJUN KIM <117059083+chorongs@users.noreply.github.com>
2023-12-19 16:08:03 +0900

    [Bronze V] Title: 시험 성적, Time: 212 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,21 +1,47 @@
 import java.util.Scanner;
 
 public class Main {
+    enum Grade {
+        A(90, 100), B(80, 89), C(70, 79), D(60, 69), F(0, 59);
+
+        private final int lower;
+        private final int upper;
+
+        Grade(int lower, int upper) {
+            this.lower = lower;
+            this.upper = upper;
+        }
+
+        public static Grade of(int score) {
+            for (Grade grade : values()) {
+                if (score >= grade.lower && score <= grade.upper) {
+                    return grade;
+                }
+            }
+            throw new IllegalArgumentException("Invalid score: " + score);
+        }
+    }
+
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-
-       int A = scanner.nextInt();
-
-       if (A >= 90) {
-           System.out.println("A");
-       } else if (A >= 80){
-           System.out.println("B");
-       } else if (A >= 70) {
-           System.out.println("C");
-       } else if (A >= 60) {
-           System.out.println("D");
-       } else {
-           System.out.println("F");
-       }
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
+        Grade grade = Grade.of(score);
+        switch (grade) {
+            case A:
+                System.out.println("A");
+                break;
+            case B:
+                System.out.println("B");
+                break;
+            case C:
+                System.out.println("C");
+                break;
+            case D:
+                System.out.println("D");
+                break;
+            case F:
+                System.out.println("F");
+                break;
+        }
     }
 }
