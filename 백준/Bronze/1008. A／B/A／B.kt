c03a872cd1aef1e5bd5bd9ce103849464f1ce07b
MyeongJun Kim <117059083+chorongs@users.noreply.github.com>
2024-02-19 13:19:20 +0900

    [Bronze V] Title: A/B, Time: 128 ms, Memory: 13744 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.kt
+++ 백준/Bronze/1008. A／B/A／B.kt
@@ -1,5 +1,9 @@
-fun main() {
-    val (a, b) = readLine()!!.split(' ').map { it.toDouble() }
-    val result = a / b
-    println(result)
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val A = scanner.nextInt()
+    val B = scanner.nextInt()
+    val result = A.toDouble() / B.toDouble()
+    println("%.9f".format(result))
 }
